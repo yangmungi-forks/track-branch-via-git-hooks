d73c10d27445d64adaf4cf79cd79eedc218d1853
Yangmun Choi <yangmungi@ucla.edu>
2012-09-28 16:43:19 -0700

    Adding multiple target match support.

--- git_post_receive.php
+++ git_post_receive.php
@@ -80,65 +80,66 @@ if (!empty($_POST['payload'])) {
                 $currdir, escapeshellarg($track_key));
             $repo_location = $trackcfg['repo_location'];
             $matching_track_key = $track_key;
+
+            // Execute the command
+            bash::execute($cmd . ' 2>&1', $output, $return_var);        
+
+            $mailto = array($admin_email);
+            $subject_part = 'Successfully run ';
+
+            // if $return_var is non-zero, then an error happened
+            // http://www.linuxtopia.org/online_books/advanced_bash_scripting_guide/exitcodes.html
+            if (0 !== $return_var) {
+                // there was an error, so email the admin
+                debug('there was an error, emailing admin: ' . $admin_email);
+
+                $subject_part = 'There was an error running ';
+            
+                $mail_content = sprintf(
+                    "return_var: %d\n\ncommand line output:\n\n%s"
+                        . "\n\njson_payload:\n\n%s", 
+                    $return_var,
+                    implode("\n", $output), 
+                    print_r($payload, true)
+                );
+            } else {
+                // update was successful, so email committer and admin
+                // get emails of committers
+                if (isset($payload->pusher->email)) {
+                    $mailto[] = $payload->pusher->email;
+                }
+
+                debug('update successful, emailing admin and pusher: ' 
+                    . implode(';', $mailto));            
+            
+                $mail_content = sprintf(
+                    "Updated repo at %s with command `%s`.\n"
+                        . "Settings: %s.\n"
+                        . "Output of the command:\n\n%s", 
+                    $repo_location,
+                    $cmd,
+                    print_r($tracking_rules[$matching_track_key], true),
+                    implode("\n", $output)
+                );
+            }
+      
+            // TODO make an email manager
+            $sent_email = mail(
+                implode(',', $mailto),
+                "git_post_receive.php : $subject_part $cmd",
+                $mail_content
+            );
+
+            if (empty($sent_email)) {
+                error_log(
+                    'Could not send email notification for git_post_receive'
+                );
+            }
         }
     }
 
     if (!$cmd) {
         debug('payload matched no rules.');
-    } else {
-        debug('executing command: ' . $cmd);
-        // Redirect STDERR output to STDOUT for email
-        exec($cmd . ' 2>&1', $output, $return_var);        
-        debug('cmd output: ' . implode("\n", $output));
-
-        $mailto = array($admin_email);
-        $subject_part = 'Successfully run ';
-
-        // if $return_var is non-zero, then an error happened
-        // http://www.linuxtopia.org/online_books/advanced_bash_scripting_guide/exitcodes.html
-        if (0 !== $return_var) {
-            // there was an error, so email the admin
-            debug('there was an error, emailing admin: ' . $admin_email);
-
-            $subject_part = 'There was an error running ';
-            
-            $mail_content = sprintf(
-                "return_var: %d\n\ncommand line output:\n\n%s"
-                    . "\n\njson_payload:\n\n%s", 
-                $return_var,
-                implode("\n", $output), 
-                print_r($payload, true)
-            );
-        } else {
-            // update was successful, so email committer and admin
-            // get emails of committers
-            if (isset($payload->pusher->email)) {
-                $mailto[] = $payload->pusher->email;
-            }
-
-            debug('update successful, emailing admin and pusher: ' 
-                . implode(';', $mailto));            
-            
-            $mail_content = sprintf(
-                "Updated repo at %s with command `%s`.\n"
-                    . "Settings: %s.\n"
-                    . "Output of the command:\n\n%s", 
-                $repo_location,
-                $cmd,
-                print_r($tracking_rules[$matching_track_key], true),
-                implode("\n", $output)
-            );
-        }
-        
-        $sent_email = mail(
-            implode(',', $mailto),
-            "git_post_receive.php : $subject_part $cmd",
-            $mail_content
-        );
-
-        if (empty($sent_email)) {
-            error_log('Could not send email notification for git_post_receive');
-        }
     }
 } else {
     debug('no payload found');
